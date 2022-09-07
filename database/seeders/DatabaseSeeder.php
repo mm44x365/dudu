fb121412996812215ed9dfe7cfad867d2da374c6
mm44x365 <maxtkj@outlook.com>
2022-09-07 14:52:04 +0700

    seeder permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserTableSeeder::class,
             CategoryTableSeeder::class,
             TagTableSeeder::class,
+            PermissionTableSeeder::class,
         ]);
     }
 }
